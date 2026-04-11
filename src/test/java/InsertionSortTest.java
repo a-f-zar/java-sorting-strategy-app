c62db299825b60edb6606b8cbabfb0c6f1b659f2
Rim Gabnasirov <Gabnasirov@bk.ru>
2026-04-11 21:52:59 +0500

    feature/JSSA-4<Divided sorting tests into different classes>

--- src/test/java/InsertionSortTest.java
+++ src/test/java/InsertionSortTest.java
@@ -1,5 +1,4 @@
 import com.aston.context.Sorter;
-import com.aston.context.strategy.BubbleSortStrategy;
 import com.aston.context.strategy.InsertionSortStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,34 +8,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class SorterTest {
-    @Test
-    @DisplayName("BubbleSort: сортировка по возрастанию")
-    void bubbleSortTest() {
-        Sorter<Integer> sorter = new Sorter<>();
-        sorter.setStrategy(new BubbleSortStrategy<>());
-
-        List<Integer> input = List.of(4, 6, 7, 8, 3);
-        List<Integer> expected = List.of(3, 4, 6, 7, 8);
-
-        List<Integer> result = sorter.sort(input, Integer::compareTo);
-
-        assertEquals(expected, result, "BubbleSort should sort correctly");
-    }
-
-    @Test
-    @DisplayName("BubbleSort: сортировка по убыванию")
-    void bubbleSortReverseTest() {
-        Sorter<Integer> sorter = new Sorter<>();
-        sorter.setStrategy(new BubbleSortStrategy<>());
-
-        List<Integer> input = List.of(1, 2, 3, 4, 5);
-        List<Integer> expected = List.of(5, 4, 3, 2, 1);
-
-        List<Integer> result = sorter.sort(input, (a, b) -> b - a);
-
-        assertEquals(expected, result, "BubbleSort should sort in reverse order");
-    }
+public class InsertionSortTest {
 
     @Test
     @DisplayName("InsertionSort: сортировка по возрастанию")
